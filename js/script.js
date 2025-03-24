fc5038e4b351250f4c6cea332b2b5375a698574b
RickNon <recutenonon22@gmail.com>
2025-03-24 11:25:28 +0900

    Add multilingual support and site styling

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('languageSwitcher').addEventListener('change', function() {
+    var selectedLang = this.value;
+    var elements = document.querySelectorAll('.lang');
+    elements.forEach(function(element) {
+        if (element.classList.contains(selectedLang)) {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
+    });
+});
